ed2fc0e7dff9db3715c6ec2941c9465eef8d58b5
MAHENDRA TEKUDE <129305588+MahiTek007@users.noreply.github.com>
2023-11-04 07:37:50 +0000

    js Datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all js code as newer version
+
+// alert( 3 + 3);   // We are  using node.js , not browser
+
+ console.log(3+3);
+//code readability is should be heigh.
+
+ console.log("Mahendra");
+
+
+ let name="Mahendra";   //String
+ let age=23;
+ let isLoggedIn=false;
+
+
+ // Primitive DataType:
+// Number => 2to power 53
+//Bigint =>
+//string => its define using "" and ''
+//boolean=> true /false
+//null => it's a standlone value and representation of Empty Value
+//undefined => We declare a veriable but its not initialize
+//symbol => its use to unique component
+
+
+//Non Primitive DataType:
+//Object
+
+console.log(typeof null); // Object
+console.log(typeof undefined)//undefined
