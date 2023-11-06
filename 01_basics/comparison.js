71f72785fbaa2de37dab870accb900740b98eba4
MAHENDRA TEKUDE <129305588+MahiTek007@users.noreply.github.com>
2023-11-06 10:23:38 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,19 @@
+// console.log(2==1);
+// console.log(30>20);
+// console.log(2!=2);
+// console.log(3<10);
+// console.log(3>10);
+// console.log("2">10); //true
+// console.log("03"<1);   //false;
+
+// console.log(null>0);   //false
+// console.log(null==0);   //false
+// console.log(null>=0);   //true
+
+// console.log(undefined==0); //false
+// console.log(undefined>=0); //false
+// console.log(undefined>0); //false
+// console.log(undefined<0); //false      // == Operator is used to compare two values without data type.But it makes conversion of same data type.
+
+//================================================================
+// console.log("2"===2);  === Operator is used to compare two values with data type.
