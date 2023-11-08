3e8b77950d1af39e735d20a53cc14bfc84f14399
MAHENDRA TEKUDE <129305588+MahiTek007@users.noreply.github.com>
2023-11-08 08:49:31 +0000

    Object Properties

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -4,7 +4,7 @@
 
 // Object.create
 
-let mySymbol= Symbol("Department Number:");
+const mySymbol=  Symbol("Department Number");
 
 const app = {           // Object is nothing but a key value pair.
     name: "Mahendra",
@@ -25,4 +25,68 @@ console.log(`My email is ${app.email}`);
 console.log(`My city is ${app.city}`);
 console.log(`My country is ${app.country}`);
 console.log(`My loginLastedTime is ${app.loginLastedTime}`);
-console.log(`My ${mySymbol} is ${app[mySymbol]}`);
+console.log(`My ${mySymbol.toString()} is ${app[mySymbol]}`);
+
+// Object.keys
+
+console.log(Object.keys(app));
+
+// Object.values
+
+console.log(Object.values(app));
+
+// Object.entries
+
+console.log(Object.entries(app));
+
+// Object.assign
+
+const newApp = Object.assign({},app);
+
+console.log(newApp);
+
+// Object.freeze
+
+Object.freeze(newApp);
+
+console.log(newApp);
+
+// Object.seal
+
+Object.seal(newApp);
+
+console.log(newApp);
+
+// Object.is
+
+console.log(Object.is(newApp,app));
+
+// Object.isExtensible
+
+console.log(Object.isExtensible(newApp));           
+
+// Object.preventExtensions
+
+Object.preventExtensions(newApp);
+
+console.log(Object.isExtensible(newApp));
+
+// Object.getOwnPropertyDescriptor
+
+console.log(Object.getOwnPropertyDescriptor(newApp,"name"));
+
+// Object.getOwnPropertyNames
+
+console.log(Object.getOwnPropertyNames(newApp));
+
+// Object.getOwnPropertySymbols
+
+console.log(Object.getOwnPropertySymbols(newApp));
+
+// Object.isSealed
+
+console.log(Object.isSealed(newApp));
+
+// Object.isFrozen
+
+console.log(Object.isFrozen(newApp));
